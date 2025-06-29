6ef739ed7b5d859f9b2aec1bed06233016d89322
zzz <zzz@zzz>
2025-06-29 17:49:58 +0800

    fix build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
+    // Removed deprecated appDir experimental option
 }
 
 module.exports = nextConfig
